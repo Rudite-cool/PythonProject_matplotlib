8381d246f233105d705c72541345e2b243bdf4bf
Rudite-cool <rudulis@gmail.com>
2025-07-02 13:13:08 +0300

    matplotlib

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ x = [2,5,8]
 y = [2,5,6]
 fig, ax = plt.subplots()
 ax.plot(x,y,color= 'blue',marker = 's')
-ax.set_xlabel("X time")
-ax.set_ylabel("Y counts")
+ax.set_xlabel("X time", fontsize=14)
+ax.set_ylabel("Y counts", fontsize=14)
 plt.show()
 
 
