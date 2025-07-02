575c3e7d3ab46b6cfdb9022d5e2a20942f154d21
Rudite-cool <rudulis@gmail.com>
2025-07-02 14:22:56 +0300

    matplotlib

--- main.py
+++ main.py
@@ -29,21 +29,46 @@ from matplotlib import pyplot as plt
 # atvaizduokite x kvadrate,x ⋅ 3,x ⋅ a, čia a - įvedamas vartotojo.
 # Panaudokite skirtingas spalvas, linijų tipus.
 
-x = np.array([1,2,3,4,5,6,7,8,9])
-a = float(input("enter a: "))
-fig, axs = plt.subplots(3, 1, figsize=(4, 8))
+# x = np.array([1,2,3,4,5,6,7,8,9])
+# a = float(input("enter a: "))
+# fig, axs = plt.subplots(3, 1, figsize=(4, 8))
+#
+# axs[0].plot(x, x**2, color='pink', linestyle='-', marker='s')
+# axs[0].set_title('x²')
+# axs[0].grid(True)
+#
+# axs[1].plot(x, x*3, color='blue', linestyle='--', marker='s')
+# axs[1].set_title('x * 3')
+# axs[1].grid(True)
+#
+# axs[2].plot(x, x*a, color='green', linestyle=':', marker='s')
+# axs[2].set_title(f'x * {a}')
+# axs[2].grid(True)
+# plt.tight_layout()
+# plt.show()
 
-axs[0].plot(x, x**2, color='pink', linestyle='-', marker='s')
-axs[0].set_title('x²')
-axs[0].grid(True)
+# task2 -----Duoti sąrašai x = [1,2,3,4,5], y1=[2,2,0,0,2], y2 = [4,3,2,1,-1], y3 =
+# [2,4,9,16,25], y4 = [-1,1,-1,1,-1]
+# Atvaizduokite viename lange, skirtinguose grafikuose x~y1, x~y2,
+# x~y3, x~y4 grafikus. Pirmasis grafikas - linijinis, antrasis - taškinis,
+# trečiasis - linija su duomenų taškais, ketvirtasis - brūkšninis.
+# Spalvos visų turi būti skirtingos. Grafikai, ašys turi turėti
+# pavadinimus.
 
-axs[1].plot(x, x*3, color='blue', linestyle='--', marker='s')
-axs[1].set_title('x * 3')
-axs[1].grid(True)
+x = [1,2,3,4,5]
+y1 = [2,2,0,0,2]
+y2= [4,3,2,1,-1]
+y3 = [2,4,9,16,25]
+y4 = [-1,1,-1,1,-1]
 
-axs[2].plot(x, x*a, color='green', linestyle=':', marker='s')
-axs[2].set_title(f'x * {a}')
-axs[2].grid(True)
+fig, ax = plt.subplots()
 
-plt.tight_layout()
+plt.plot(x, y1,color= 'red',label='x~y1' )
+plt.title('Graph y1 pret x')
+
+ax.scatter(x, y2,color= 'blue', label='x~y2' )
+plt.title('Graph y2 pret x')
+ax.tick_params(axis='x')
+ax.tick_params(axis='y')
+ax.legend()
 plt.show()
