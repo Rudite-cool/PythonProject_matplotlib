c39383ffe9736397daa1658dc01f25a60b126a00
Rudite-cool <rudulis@gmail.com>
2025-07-03 13:03:06 +0300

    matplotlib

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ from matplotlib import pyplot as plt
 x = [1,2,3,5,8]
 explode = [0,0,0,0,0.25]
 fig, ax = plt.subplots()
-ax.pie(x, labels=x,explode=explode, autopct='%1.2f%%',shadow=True)
+ax.pie(x, labels=x,autopct='%1.2f%%')
 plt.show()
 
 # -------------
